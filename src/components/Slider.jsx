d22cde0693749db3c93d098d00789458333fe9ee
Algerina <kh.amelle@gmail.com>
2023-05-29 15:58:48 +0100

    Create Slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 import CardSlider from './CardSlider';
 
 export default function Slider({movies}) {
+const getMoviesFromRange = (from, to) => {
+    return movies.slice(from, to);
+}
+    
   return <div>
-        <CardSlider title='Trending Now' />
+        <CardSlider title='Trending Now' data={getMoviesFromRange(0, 10)} />
+        <CardSlider title='Trending Now' data={getMoviesFromRange(10, 20)} />
+        <CardSlider title='Trending Now' data={getMoviesFromRange(20, 30)} />
+        <CardSlider title='Trending Now' data={getMoviesFromRange(30, 40)} />
+        <CardSlider title='Trending Now' data={getMoviesFromRange(40, 50)} />
     </div>;
   
 }
