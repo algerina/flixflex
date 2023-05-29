657c12ef892515bfb8ae2499a1393aabff9e954d
Algerina <kh.amelle@gmail.com>
2023-05-29 08:42:48 +0100

    Style login page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -4,9 +4,10 @@ import BackgroundImage from '../components/BackgroundImage';
 import Header from '../components/Header';
 
 export default function Signup() {
-  return 
+  return ( 
     <Container>
       <BackgroundImage />
+      <div className='content'>
       <Header />
       <div className='body flex column a-center j-center'>
         <div calssName='column test flex'>
@@ -23,7 +24,22 @@ export default function Signup() {
         </div>
         <button>Log In</button>
       </div>
-      </Container>;
+      </div>
+      </Container>
+  )
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+position: relative;
+.content {
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(0,0,0,0.5);
+  height: 100vh;
+  width: 100vw;
+  display: grid;
+  grid-template-rows: 15vh 85vh;
+}
+
+`;
