2309ef09c789d93847c7e496627b0b9e9def7cf5
Algerina <kh.amelle@gmail.com>
2023-05-29 23:43:36 +0100

    Create flixflex-api and connect it to the ui

---
+++ flixflex-ui/README.md
@@ -0,0 +1,63 @@
+![](https://img.shields.io/static/v1?label=BY&message=Algerina&color=pink)
+
+# Flixflex
+
+<hr>
+
+## Description
+
+>  This project is a web application: Movie app FlixFlex. Built using React, Redux Toolkit, Firebase, Styled Components, Axios, Node.js, Express and MongoDB.
+
+ 
+<hr>
+![screenshot](./Screeshot.png)
+
+<hr>
+
+
+
+## Steps to Start the App
+Install React Dependencies
+Instal Node Dependencies
+Start Node App
+Start React App
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+The page will reload when you make changes.\
+You may also see any lint errors in the console.
+
+
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+
+### `npm run eject`
+
+**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+
+
+ 👤 **Amel Khiri**
+
+ Platform | Badge |
+ --- | --- |
+ **GitHub**  | [@Algerina](https://github.com/Algerina)
+ **LinkedIn** | [@Amel Khiri](https://www.linkedin.com/in/amel-khiri/)
+<hr>
+
+
+<hr>
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
---
+++ flixflex-ui/src/pages/TVShows.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
+import CardSlider from "../components/CardSlider";
+import { onAuthStateChanged } from "firebase/auth";
+import { firebaseAuth } from "../utils/firebase-config";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchMovies, getGenres } from "../store";
+import SelectGenre from "../components/SelectGenre";
+import Slider from "../components/Slider";
+
+function TVShows() {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const movies = useSelector((state) => state.flixflex.movies);
+  const genres = useSelector((state) => state.flixflex.genres);
+  const genresLoaded = useSelector((state) => state.flixflex.genresLoaded);
+  const dataLoading = useSelector((state) => state.flixflex.dataLoading);
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!genres.length) dispatch(getGenres());
+  }, []);
+
+  useEffect(() => {
+    if (genresLoaded) {
+      dispatch(fetchMovies({ genres, type: "tv" }));
+    }
+  }, [genresLoaded]);
+
+  const [user, setUser] = useState(undefined);
+
+  onAuthStateChanged(firebaseAuth, (currentUser) => {
+    if (currentUser) setUser(currentUser.uid);
+    else navigate("/login");
+  });
+
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+
+  return (
+    <Container>
+      <Navbar isScrolled={isScrolled} />
+      <div className="data">
+        <SelectGenre genres={genres} type="tv" />
+        {movies.length ? (
+          <>
+            <Slider movies={movies} />
+          </>
+        ) : (
+          <h1 className="not-available">
+            No TV Shows avaialble.
+          </h1>
+        )}
+      </div>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  .data {
+    margin-top: 8rem;
+    .not-available {
+      text-align: center;
+      margin-top: 4rem;
+    }
+  }
+`;
+export default TVShows;
