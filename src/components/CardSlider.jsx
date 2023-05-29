1f560f7c8e0ac6f5e1d0c2f53d5026b4f17f081a
Algerina <kh.amelle@gmail.com>
2023-05-29 16:05:47 +0100

    Add card component

--- src/components/CardSlider.jsx
+++ src/components/CardSlider.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Card from './Card';
 
-export default function CardSlider() {
-  return (
-    <div>CardSlider</div>
-  )
+export default function CardSlider({data, title}) {
+  return <div>
+{
+    data.map((movie, index)=> {
+        return <Card movieData={movie} index={index} key={movie.id} />
+    })
+}
+    </div>;
 }
